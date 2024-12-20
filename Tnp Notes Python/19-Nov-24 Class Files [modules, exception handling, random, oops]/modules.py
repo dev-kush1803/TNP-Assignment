95975b8e25f7ec5938f889049de31acb999ea524
Kush Lodhi <136368394+dev-kush1803@users.noreply.github.com>
2024-12-20 15:55:03 +0530

    Create modules.py

---
+++ Tnp Notes Python/19-Nov-24 Class Files [modules, exception handling, random, oops]/modules.py
@@ -0,0 +1,15 @@
+# projects ->
+# skills - R&D, UI/UX, FE, BE, DB, cloud, AI/ML,DS,BigData, CY,
+# networking, DM
+# Certification
+# DATA LAKE/WAREHOUSE
+
+# COMMUNICATION
+
+#Modules
+
+city = "bhopal"
+def greet():
+    print("Welcome in Bhopal")
+
+greet()
