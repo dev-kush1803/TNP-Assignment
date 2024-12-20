e91cdd38bcffb80596e6c77a92b2edff8ed5ec1e
Kush Lodhi <136368394+dev-kush1803@users.noreply.github.com>
2024-12-20 15:47:30 +0530

    Create demo.py

---
+++ Tnp Notes Python/22-Oct-2024 (File handling)/demo.py
@@ -0,0 +1,256 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+NAME: John
+HELLO
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+Traceback (most recent call last):
+  File "E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py", line 14, in <module>
+    file = open('example.txt')
+FileNotFoundError: [Errno 2] No such file or directory: 'example.txt'
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+Traceback (most recent call last):
+  File "E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py", line 14, in <module>
+    file = open('example.txt','r')
+FileNotFoundError: [Errno 2] No such file or directory: 'example.txt'
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+<_io.TextIOWrapper name='example.txt' mode='w' encoding='cp65001'>
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+<_io.TextIOWrapper name='example.txt' mode='w' encoding='cp65001'>
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+<_io.TextIOWrapper name='example.txt' mode='w' encoding='cp65001'>
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+<_io.TextIOWrapper name='example.txt' mode='w' encoding='cp65001'>
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+<_io.TextIOWrapper name='example.txt' mode='w' encoding='cp65001'>
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+Traceback (most recent call last):
+  File "E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py", line 15, in <module>
+    file.write("BYE","HELLO")
+TypeError: write() takes exactly one argument (2 given)
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+<_io.TextIOWrapper name='example.txt' mode='w' encoding='cp65001'>
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+Traceback (most recent call last):
+  File "E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py", line 15, in <module>
+    file.write(["BYE HELLO","HI"])
+TypeError: write() argument must be str, not list
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+<_io.TextIOWrapper name='example.txt' mode='w' encoding='cp65001'>
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+<_io.TextIOWrapper name='example.txt' mode='a' encoding='cp65001'>
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+<_io.TextIOWrapper name='example.txt' mode='a' encoding='cp65001'>
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+<_io.TextIOWrapper name='example.txt' mode='a' encoding='cp65001'>
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+HELLO
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+HELLO
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+HELLO
+Line number 1
+Line number 2
+Line number 3
+Line number 4
+Line number 5
+Line number 6
+Line number 7
+Line number 8
+Line number 9
+Line number 1
+Line number 2
+Line number 3
+Line number 4
+Line number 5
+Line number 6
+Line number 7
+Line number 8
+Line number 9
+
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+Line number 1
+Line number 2
+Line number 3
+Line number 4
+Line number 5
+Line number 6
+Line number 7
+Line number 8
+Line number 9
+Line number 10
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+L
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+0
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+
+
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+Line number 1
+
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+L
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+1
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+10
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your name: john
+Enter your enrollment: 0103EC212121
+Enter your college: LNCT
+Enter your password: 123456
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your name: mac
+Enter your enrollment: 0103CS212123
+Enter your college: LNCT
+Enter your password: 123456
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your name: sam
+Enter your enrollment: 0176AI212012
+Enter your college: LNCTE
+Enter your password: 123456
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your name: john
+Enter your enrollment: 0103IT212121
+Enter your college: LNCT
+Enter your password: 123456
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your name: sam
+Enter your enrollment: 0176AL212123
+Enter your college: LNCTE
+Enter your password: 123546
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your name: macc
+Enter your enrollment: 0157IT212125
+Enter your college: LNCTS
+Enter your password: 123456
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your name: 
+Traceback (most recent call last):
+  File "E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py", line 46, in <module>
+    register()
+  File "E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py", line 36, in register
+    na = input("Enter your name: ")
+KeyboardInterrupt
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your Enrollment: 0103IT212121
+Enter your password: 123456
+LOgin Successful
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your Enrollment: 0103IT212121
+Enter your password: edrtfgyhujiko
+Wrong password
+Wrong username
+Wrong username
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your Enrollment: 0103IT212121
+Enter your password: 123456
+LOgin Successful
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your Enrollment: 0103IT212121
+Enter your password: tghj
+Wrong password
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your Enrollment: fgyhjk
+Wrong username
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your Enrollment: 0157IT212125
+Wrong username
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your Enrollment: 0157IT212125
+Wrong username
+Wrong username
+Enter your password: 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your Enrollment: 0157IT212125
+Enter your password: 123456
+Wrong Credentials
+Wrong Credentials
+login successful
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your Enrollment: 0157IT212125
+Enter your password: 123456
+login successful
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your Enrollment: 
+Enter your password: 
+Wrong Credentials
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your Enrollment: 0157IT212125
+Enter your password: 54321
+Wrong Credentials
+>>> 
+= RESTART: E:/LNCT/Classes/2024/IT_Training_5th_sem_2_sep/Batch_9_[TTS]_LNCTE_CSE_LNCT_AIML_CSE/fileHandling.py
+['Line number 1\n', 'Line number 2\n', 'Line number 3\n', 'Line number 4\n', 'Line number 5\n', 'Line number 6\n', 'Line number 7\n', 'Line number 8\n', 'Line number 9\n', 'Line number 10']
+Enter your Enrollment: asdfg
+Enter your password: dfg
+Wrong Credentials
+>>> 
