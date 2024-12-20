8c06daf8a322669c8c3a78f846265d329a1ba1bf
Kush Lodhi <136368394+dev-kush1803@users.noreply.github.com>
2024-12-20 16:24:33 +0530

    Create loop_programs_8_oct.py

---
+++ Tnp Notes Python/08-10-24 Comprehension_Loop_Programs/loop_programs_8_oct.py
@@ -0,0 +1,27 @@
+#WAP to reverse a string
+inp1 = "hello"
+out1 = "olleh"
+res = ''
+for i in inp1: # e
+    res = i + res # olleh
+
+print(res)
+
+#WAP to check given string is palindrom or not
+if inp1 == res:
+    print("Given string is palindrom")
+else:
+    print("Given string is not palindrom")
+
+
+#WAP to count vowels and consonant in given string
+st = "Hello I Am leaning Python in punjab"
+x = 0
+y = 0
+for i in st.lower():
+    if i.isalpha():
+        if i in 'aeiou':
+            x += 1
+        else:
+            y += 1
+
